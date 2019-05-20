71b1aa1ef6c5ceec92c2778d7f51395d107b07ec
YouRa <allakhverdovyuriy@gmail.com>
2019-05-20 19:55:42 +0300

    Final

---
+++ app/src/main/java/com/android/uraall/playingaudio/MainActivity.java
@@ -0,0 +1,73 @@
+package com.android.uraall.playingaudio;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    Button button;
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("Progress changed: ", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        button = findViewById(R.id.playButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    pause();
+                } else {
+                    play();
+                }
+            }
+        });
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+    }
+
+    public void play() {
+        mediaPlayer.start();
+        button.setText("Pause");
+    }
+
+    public void pause() {
+        mediaPlayer.pause();
+        button.setText("Play");
+    }
+}
